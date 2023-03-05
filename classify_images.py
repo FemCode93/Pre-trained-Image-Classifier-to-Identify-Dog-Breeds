2fd669d0b95288f803229f7738c68444758dd642
FemCode93 <121200215+FemCode93@users.noreply.github.com>
2023-03-05 18:43:16 +0400

    Add files via upload

---
+++ classify_images.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
+#                                                                             
+import os.path
+from classifier import classifier 
+
+def classify_images(images_dir, results_dic, model):
+
+    # classifier function
+    for key,value in results_dic.items():
+        model_label = os.path.join(images_dir + '/' + key)
+        
+        model_label = model_label.lower().strip()
+              
+       # defines truth as pet image label
+        truth = results_dic[key][0]
+        
+        if truth in model_label:
+            results_dic[[key][0]].extend([model_label, 1])
+        else:
+            results_dic[[key][0]].extend([model_label, 0])
